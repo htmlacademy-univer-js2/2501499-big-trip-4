10f6321a6c10c8b2f49d9a5280631481b59ee88f
Kdzefuf <115563854+Kdzefuf@users.noreply.github.com>
2024-03-31 01:37:00 +0500

    upd

--- src/view/filter-view.js
+++ src/view/filter-view.js
@@ -1,15 +1,13 @@
 import AbstractView from '../framework/view/abstract-view.js';
 
 function createFilterItemsTemplate({filters}) {
-  const filterItems = filters.map((filter) => {
-    return (
-      `<div class="trip-filters__filter">
+  const filterItems = filters.map((filter) => (
+    `<div class="trip-filters__filter">
         <input id="filter-${filter.type}" class="trip-filters__filter-input  visually-hidden" type="radio" name="trip-filter" value="${filter.type}"
         ${(filter.defaultSelected ? 'checked' : '')} ${(filter.hasPoints) ? '' : 'disabled'}>
         <label class="trip-filters__filter-label" for="filter-${filter.type}">${filter.type}</label>
       </div>`
-    );
-  }).join('');
+  )).join('');
 
   return filterItems;
 }
@@ -29,7 +27,7 @@ export default class FilterView extends AbstractView {
   #itemChangeHandler = (event) => {
     event.preventDefault();
     this.#onItemChange?.(event.target.dataset.value);
-  }
+  };
 
   constructor(filters, onItemChange) {
     super();
